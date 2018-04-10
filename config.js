73661ffa81657c70f4da464b3a4516b957113d7d
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-04-10 14:15:02 +0200

    Add Google auth.

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID':'624449987645-65frqd1ldj4tkrr2akahdlp51tqc7e6k.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET':'kNDJOah4rcwV90WU0wJH2uFO',
+  'Callback_URL': 'http://localhost:3000/auth/google/callback'
+};
